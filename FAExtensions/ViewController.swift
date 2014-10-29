f06270904ec1ebf3b88afdb9aec5f19b478ecda5
xcstream <xcstream@vip.qq.com>
2014-10-30 02:09:06 +0800

    Initial Commit

---
+++ FAExtensions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FAExtensions
+//
+//  Created by xcstream on 10/30/14.
+//  Copyright (c) 2014 xcstream. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
